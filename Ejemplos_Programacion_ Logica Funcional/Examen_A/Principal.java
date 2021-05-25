ee275b4d1c29eb3ab88e7139be95102fce932371
AbrahamEUSA <78749393+AbrahamEUSA@users.noreply.github.com>
2021-05-25 10:04:40 -0500

    Update Principal.java

--- Ejemplos_Programacion_ Logica Funcional/Examen_A/Principal.java
+++ Ejemplos_Programacion_ Logica Funcional/Examen_A/Principal.java
@@ -12,10 +12,11 @@ public class Principal {
         pe.mensajeHola();
         
         pe = (a1,a2,a3) ->  System.out.println("5- :"+(a1 + (a2 * a3)) );
-	    pe.operacionPrueba(5,10,5);
+	   
       	pe.operacionPrueba(1,PruebaExamen.operacionPrueba(1, 2),PruebaExamen.operacionPrueba( 1,2));
        
-        //Principal.miMetodo(pe);//tengo duda ya que con esta linea imprime 9
+        
+	Principal.miMetodo(pe);//tengo duda ya que con esta linea imprime 9
         Principal.miMetodo((a1,  a2,  a3)->System.out.println("7- :"+(a1 + ((double)a2 / a3))));
         pe=((a1,  a2,  a3)->System.out.println("8- :"+(a1 + ((double)a2 / a3))));
         miMetodo(pe,2,2,2); 
